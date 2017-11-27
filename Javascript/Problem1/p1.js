f9b404a128237156db3c9e61c370300bf011bafc
Prabodh Meshram <prabodh.meshram7@gmail.com>
2017-11-27 10:42:18 +0530

    Added P1 solution in Node Js

---
+++ Javascript/Problem1/p1.js
@@ -0,0 +1,25 @@
+'use strict'
+let findMultiplesOfThreeAndFive = (range)=> {
+    if(isNaN(range) || 
+        !Number.isInteger(range) ||
+        range <=3
+    )
+        return 0;
+
+    
+    let sumOfMultiples = 0;
+
+    for(let number=0; number< range; number++){
+
+        if(number%3 == 0){
+            sumOfMultiples += number;
+        }else if(number %5 == 0){
+            sumOfMultiples += number;
+        }
+    }
+
+    return sumOfMultiples;
+    
+}
+
+module.exports = findMultiplesOfThreeAndFive;

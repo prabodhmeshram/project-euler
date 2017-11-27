cca42dbc522a37581813ed5c628c6ed5af68e447
Prabodh Meshram <prabodh.meshram7@gmail.com>
2017-11-27 22:45:54 +0530

    Test Cases updated

--- Javascript/test-suite.js
+++ Javascript/test-suite.js
@@ -7,19 +7,26 @@ const findMultiplesOfThreeAndFive = require('./Problem1/p1').findMultiplesOfThre
 describe('Project Euler Problem 1 - Summ of Multiple of 3 and 5', ()=>{
 
     describe('Invalid test cases', ()=>{
-        it('should return 0 for Invalid data', ()=>{
+        it('should return 0 for String', ()=>{
             expect(findMultiplesOfThreeAndFive('Grab')).to.equal(0);
         })
 
-        it('should return 0 for Invalid number', () => {
+        it('should return 0 for Non Integer', () => {
+            expect(findMultiplesOfThreeAndFive('-10.9')).to.equal(0);
+            expect(findMultiplesOfThreeAndFive('10.9')).to.equal(0);
+        })
+
+        it('should return 0 for Invalid Integer', () => {
             expect(findMultiplesOfThreeAndFive(-10)).to.equal(0);
             expect(findMultiplesOfThreeAndFive(3)).to.equal(0);
         })
+    })
 
-        it('should return proper output', () => {
+    describe('Valid test Cases', ()=>{
+        it('should return proper output for Valid Integer', () => {
             expect(findMultiplesOfThreeAndFive(4)).to.equal(3);
             expect(findMultiplesOfThreeAndFive(10)).to.equal(23);
         })
-        
     })
+
 });

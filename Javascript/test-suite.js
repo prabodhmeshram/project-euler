bcde67472233b3fe0191be58e8f5524e851f16f9
Prabodh Meshram <prabodh.meshram7@gmail.com>
2017-11-28 23:20:42 +0530

    Added Test Suites for Euler Prob 2 : Js

--- Javascript/test-suite.js
+++ Javascript/test-suite.js
@@ -2,9 +2,10 @@
 
 const expect = require('chai').expect;
 const findMultiplesOfThreeAndFive = require('./Problem1/p1').findMultiplesOfThreeAndFive;
+const findSumOfEvenFibonacci = require('./Problem2/p2').findSumOfEvenFibonacci;
 
 
-describe('Project Euler Problem 1 - Summ of Multiple of 3 and 5', ()=>{
+describe('Project Euler Problem 1 - Sum of Multiple of 3 and 5', ()=>{
 
     describe('Invalid test cases', ()=>{
         it('should return 0 for String', ()=>{
@@ -26,6 +27,35 @@ describe('Project Euler Problem 1 - Summ of Multiple of 3 and 5', ()=>{
         it('should return proper output for Valid Integer', () => {
             expect(findMultiplesOfThreeAndFive(4)).to.equal(3);
             expect(findMultiplesOfThreeAndFive(10)).to.equal(23);
+        })
+    })
+
+});
+
+describe('Project Euler Problem 2 - Sum of even Fibonacci Series number', () => {
+
+    describe('Invalid test cases', () => {
+        it('should return 0 for String', () => {
+            expect(findSumOfEvenFibonacci('Grab')).to.equal(0);
+        })
+
+        it('should return 0 for Non Integer', () => {
+            expect(findSumOfEvenFibonacci('-10.9')).to.equal(0);
+            expect(findSumOfEvenFibonacci('10.9')).to.equal(0);
+        })
+
+        it('should return 0 for Invalid Integer', () => {
+            expect(findSumOfEvenFibonacci(-10)).to.equal(0);
+            expect(findSumOfEvenFibonacci(1)).to.equal(0);
+        })
+
+    })
+
+    describe('Valid test Cases', () => {
+        it('should return proper output for Valid Integer', () => {
+            expect(findSumOfEvenFibonacci(4)).to.equal(2);
+            expect(findSumOfEvenFibonacci(10)).to.equal(10);
+            expect(findSumOfEvenFibonacci(40)).to.equal(44);
         })
     })
 

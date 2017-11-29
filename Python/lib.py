0c716fc7f7fe1aad334c0372d50c393c0ebcb01a
Prabodh Meshram <prabodh.meshram7@gmail.com>
2017-11-29 23:06:33 +0530

    Added Lib Fuction for Prime number list and check if a number is positive Integer

--- Python/lib.py
+++ Python/lib.py
@@ -39,3 +39,69 @@ def get_fibonacci_for_range(range):
 
     return fibo_list
 
+
+def get_prime_numbers_for_range(num_range):
+    """
+    Get Prime numbers series
+
+    Function to take num_range as integer and return prime numbers till number
+
+    Parameters
+    ----------
+    num_range : int
+        Range of number
+
+    Returns
+    -------
+    list
+
+    Author
+    ------
+    Prabodh M
+
+    Date
+    ------
+    29 Nov 2017
+
+    """
+
+    prime_list = []
+
+    prime_list.append(2)
+
+    for num in range(3, num_range+1):
+        isPrime = True
+        for prime in prime_list:
+            if num % prime is 0:
+                isPrime = False
+                break
+        if isPrime:
+            prime_list.append(num)
+
+    return prime_list
+
+
+def isValidPositiveInteger(number):
+    """
+    Check if number is valid positive integer
+
+    Parameters
+    ----------
+    number : int
+
+    Returns
+    -------
+    bool
+
+    Author
+    ------
+    Prabodh M
+
+    Date
+    ------
+    29 Nov 2017
+
+    """
+    if isinstance(number, int) and number > 0:
+        return True
+    return False
